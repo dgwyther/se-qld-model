79728a3245beec007414b3395778f71b2b07f827
dgwyther <david.gwyther@gmail.com>
2023-08-01 11:48:50 +1000

    add new grid making fcns; tweak mask

--- src/functions/GridAdjustments.py
+++ src/functions/GridAdjustments.py
@@ -36,3 +36,55 @@ def adjustMask(mask_new,mask_alter,eta,xi):
     """    
     mask_new[eta,xi]=mask_alter
     return mask_new
+
+def setBathy(h_new,h_alter,eta,xi):
+    """
+    function setBathy(h,h_alter,eta,xi
+    
+    usage:
+    h_new = setBathy(h,h_alter,eta,xi)
+    
+    This function adjusts h[eta,xi] to h_alter.
+    To pass a range, use the slice literal:
+    eta=slice(100,132)
+
+    """
+    h_new[eta,xi] = h_alter
+    return h_new
+
+
+def smoothBathyPatch(h_new,eta,xi):
+    """
+    function smoothBathyPatch(h_new,eta,xi)
+    
+    usage:
+    h_new = smoothBathyPatch(h_new,eta,xi)
+    
+    
+
+    """
+        # define inpaint_nans
+    from scipy.signal import convolve2d
+    import numpy as np
+    def inpaint_nans(inField):
+        im = inField.copy()
+        import scipy
+        ipn_kernel = np.array([[1,1,1],[1,0,1],[1,1,1]]) # kernel for inpaint_nans
+        nans = np.isnan(im)
+        while np.sum(nans)>0:
+            im[nans] = 0
+            vNeighbors = convolve2d((nans==False),ipn_kernel,mode='same',boundary='symm')
+            im2 = convolve2d(im,ipn_kernel,mode='same',boundary='symm')
+            im2[vNeighbors>0] = im2[vNeighbors>0]/vNeighbors[vNeighbors>0]
+            im2[vNeighbors==0] = np.nan
+            im2[(nans==False)] = im[(nans==False)]
+            im = im2
+            nans = np.isnan(im)
+        return im
+
+
+    h_new[eta,xi] = np.NaN
+    h_new = inpaint_nans(h_new)
+    
+    return h_new
+
